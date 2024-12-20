50e558fc87968e126d2e639e9bf4477c7c6cde45
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-12-19 20:47:49 -0600

    Version 0.2.7

--- constants.lua
+++ constants.lua
@@ -1,54 +1,24 @@
--- Constants shared between settings.lua, data.lua, and control.lua.
--- This file contains allowed image types, excluded entity types, button colors, and utility functions.
 local constants = {}
 
--- Mod name
 constants.mod_name = "big-pink-eraser"
-
--- The mod path used to access assets like icons and graphics
 constants.mod_path = "__" .. constants.mod_name .. "__/"
-
--- Base path to the icons folder.
 constants.icon_path = constants.mod_path .. "graphics/icons/"
-
--- Path to the image icon
-constants.tool = constants.icon_path .. "clip-art-tool.png"
-
--- Table of allowed button colors for the Big Pink Eraser shortcut
+constants.tool_icon = constants.icon_path .. "clip-art-tool.png"
+constants.icon_size = 32
+constants.icon_size_small = 24
 constants.allowed_button_colors = { "gray", "red", "green", "blue" }
-
--- Default button color for the Big Pink Eraser shortcut
 constants.default_button_color = "gray"
-
--- Table of allowed image types for the Big Pink Eraser tool
 constants.allowed_image_types = { "image", "clip-art-pink", "clip-art-white", "clip-art-black" }
-
--- Default image type for the Big Pink Eraser tool
 constants.default_image_type = "clip-art-black"
-
--- Forces used for filtering in entity searches
 constants.allowed_forces = { "neutral", "player" }
-
--- The color used for the selection box
 constants.white = { r = 1, g = 1, b = 1, a = 1 }
-
--- Sort order for the selection tool
 constants.tool_order = "c[automated-construction]-a[big-pink-eraser]"
-
--- Sort order for the shortcut
 constants.shortcut_order = "a[tools]-c[big-pink-eraser]"
-
--- Small icon size
-constants.icon_size_small = 32
-
--- List of entity types that are excluded from destruction by the Big Pink Eraser tool
 constants.excluded_types_list = { "character", "cliff", "decorative", "fish", "resource", "simple-entity", "tile", "tree" }
 
--- Create the excluded_types_table table from the excluded_strings list
 local function create_excluded_types_table()
     local excluded_types_table = {}
 
-    -- Loop through each string in constants.excluded_list
     for _, type in ipairs(constants.excluded_types_list) do
         excluded_types_table[type] = true
     end
--- settings.lua
+++ settings.lua
@@ -1,16 +1,5 @@
--- Require the constants.lua file to access shared constants.
--- This includes button colors and image types.
 local constants = require("constants")
 
--- Defines a startup setting for the Big Pink Eraser shortcut button style.
--- Allows the player to choose the button color, defaulting to gray.
--- Defines a startup setting for the Big Pink Eraser shortcut button style.
--- Allows the player to choose the button color, defaulting to gray.
--- @field type string The type of setting, in this case, a string-setting.
--- @field name string The internal name of the setting used to reference it in the code.
--- @field setting_type string Specifies that this is a startup setting.
--- @field default_value string The default button color ("gray"), mapped internally to "default".
--- @field allowed_values table The list of selectable colors for the shortcut button (gray, red, green, blue).
 data:extend({
     {
         type = "string-setting",
@@ -22,13 +11,6 @@ data:extend({
     }
 })
 
--- Defines a startup setting for the Big Pink Eraser button image type.
--- Allows the player to choose the button image, defaulting to an HD image.
--- @field type string The type of setting, in this case, a string-setting.
--- @field name string The internal name of the setting used to reference it in the code.
--- @field setting_type string Specifies that this is a startup setting, configured before the game starts.
--- @field default_value string The default button image type ("image").
--- @field allowed_values table The list of available button image types (image, clip-art-white, clip-art-black).
 data:extend({
     {
         type = "string-setting",
@@ -40,11 +22,6 @@ data:extend({
     }
 })
 
--- Defines a startup setting to restrict the Big Pink Eraser shortcut to admins.
--- @field type string The type of setting, in this case, a bool-setting.
--- @field name string The internal name of the setting used to reference it in the code.
--- @field setting_type string Specifies that this is a startup setting, configured before the game starts.
--- @field default_value boolean Whether the shortcut is restricted to admins by default (true).
 data:extend({
     {
         type = "bool-setting",
